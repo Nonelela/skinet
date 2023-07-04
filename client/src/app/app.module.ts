0e16958ad36097d2a9b7e5fc278443acaa793c21
https://Nonelela@bitbucket.org/neuromac/WeirMinerals.git <nonelela.baneti@gmail.com>
2023-07-04 21:51:59 +0200

    End of section 11

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http'
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -21,7 +21,9 @@ import { HomeModule } from './home/home.module';
     HomeModule
   ],
   //No need to add service here as this is now done on the service
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 //App moduule is instantiated the application starts
